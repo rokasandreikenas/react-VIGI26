cf3f838d2e37513dd4b7a7a580e8bfe173c1e4c9
Rokas Andreikenas <rokas.andreikenas@exacaster.com>
2022-09-21 20:05:23 +0300

    Add react examples

---
+++ 1_paskaita/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+// JSX struktūra kuri renderina Javascript + HTML
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>LOgo</h1>
+      </header>
+
+      <main></main>
+      <footer></footer>
+    </div>
+  );
+}
+
+export default App;
