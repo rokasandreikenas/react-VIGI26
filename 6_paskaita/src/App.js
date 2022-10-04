75e6e2f9a54ca5f6d2455a0e8d55aae99f909ad5
Rokas Andreikenas <rokas.andreikenas@exacaster.com>
2022-10-04 18:51:29 +0300

    Add localstorage example

--- 6_paskaita/src/App.js
+++ 6_paskaita/src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Todos from "./pages/Todos/Todos";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Todos />
+    </div>
+  );
 }
 
 export default App;
---
+++ 6_paskaita/src/pages/Todos/Todos.js
@@ -0,0 +1,48 @@
+import { useState, useEffect } from "react";
+import "./Todos.css";
+
+const Todos = () => {
+  const [text, setText] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const localTodos = JSON.parse(localStorage.getItem("todos"));
+
+    if (localTodos) {
+      setTodos(localTodos);
+    }
+  }, []);
+
+  const handleAddTodo = (event) => {
+    event.preventDefault();
+    if (text) {
+      setTodos((prevTodos) => [...prevTodos, text]);
+      setText("");
+      localStorage.setItem("todos", JSON.stringify([...todos, text]));
+      // ["Item", "labas"] => "["Item", ...]"
+    }
+  };
+
+  return (
+    <div className="container">
+      {todos.length} Todos
+      <form onSubmit={handleAddTodo}>
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button type="submit">Add todo</button>
+      </form>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            <h3>{todo}</h3>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Todos;
